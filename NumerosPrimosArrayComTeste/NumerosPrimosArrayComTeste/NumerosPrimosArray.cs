24ff526f94c7b42bc92bf687e4dd45cb8768e3be
gabriel <gbbatistasouza@gmail.com>
2023-04-17 15:23:25 -0300

    Verificando números primos de um array

---
+++ NumerosPrimosArrayComTeste/NumerosPrimosArrayComTeste/NumerosPrimosArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace NumerosPrimosArrayComTeste
+{
+    public static class NumerosPrimosArray
+    {
+        public static int[] GetNumerosPrimos(int[] numeros)
+        {
+            List<int> numerosPrimos = new List<int>();
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                int num = numeros[i];
+                if (IsPrimo(num))
+                {
+                    numerosPrimos.Add(num);
+                }
+            }
+            return numerosPrimos.ToArray();            
+        }
+
+        private static bool IsPrimo(int numero)
+        {
+            int qtdDivisores = 0;
+            if (numero <= 1)
+            {
+                return false;
+            }
+            for (int i = 1; i <= numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    qtdDivisores++;                    
+                }
+            }
+            if (qtdDivisores == 2)
+            {
+                return true;
+            };
+            return false;
+            
+        }
+    }
+}
